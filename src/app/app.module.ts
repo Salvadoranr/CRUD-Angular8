f7cc41c230c58ec64478e01a07526f5e2041fdf8
salvador nipitella <salvadoranr16@gmail.com>
2019-11-30 22:30:57 +0100

    adding component pokemon and pokemon list with respective route, also config for prettier plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
