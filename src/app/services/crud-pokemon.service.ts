1e4d8e275fcc6e7944a81ff65c94ca7f50568131
salvador nipitella <salvadoranr16@gmail.com>
2019-11-30 20:34:41 +0100

    create services crud, interface and mock of the api

---
+++ src/app/services/crud-pokemon.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { apiResponse } from '../api/api-mock';
+import { IPokemon } from '../interfaces/pokemon.interface';
+
+// const baseUrl = 'https://pokeapi.co/api/v2/';
+// const pokemonUrl = `${baseUrl}pokemon/`;
+// const ElectricsPokemonUrl = `${pokemonUrl}type/13`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudPokemonService {
+  public pokemonsList: IPokemon[];
+  constructor() {
+    this.pokemonsList = [];
+    this.getInitialResponse();
+  }
+
+  public getInitialResponse(): void {
+    this.pokemonsList = apiResponse.map(el => el.pokemon);
+  }
+  public getpokemonsList(): IPokemon[] {
+    return this.pokemonsList;
+  }
+}
