5336d60fd95238e21f202a0e470b8f68e9419a98
salvador nipitella <salvadoranr16@gmail.com>
2019-12-01 02:55:20 +0100

    modifying api-mock, pokemon component,pokemon list, interfaces and crud service

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { IPokemon } from 'src/app/interfaces/pokemon.interface';
 
 @Component({
   selector: 'app-pokemon',
@@ -6,11 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./pokemon.component.scss']
 })
 export class PokemonComponent implements OnInit {
-@Input() name: string;
-@Input() url: string;
-  constructor() { }
-
-  ngOnInit() {
+  @Input() pokemon: IPokemon;
+  pokemonNumber: string;
+  pokemonWeigth: string;
+  pokemonType: string;
+  pokemonSkills: string;
+  constructor() {
+    this.pokemonNumber = 'Numero';
+    this.pokemonWeigth = 'Weigth';
+    this.pokemonType = 'Type';
+    this.pokemonSkills = 'Skills';
   }
 
+  ngOnInit() {}
 }
